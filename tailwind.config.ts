8a99d9d94d430d307c8147d37f521e81aad7c85e
mugdhodzs38 <mugdhodzs38@gmail.com>
2024-10-02 07:35:23 +0000

    init, theme

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -14,6 +14,14 @@ const config: Config = {
       },
     },
   },
-  plugins: [],
+  plugins: [
+    require('daisyui'),
+  ],
+  daisyui: {
+    themes: ["light", "dark", "cupcake", "retro"],
+  },
 };
+
+
+
 export default config;
