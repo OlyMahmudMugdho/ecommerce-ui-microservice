522e7037d90a718f0b4327a8fcdd9bfd19181c59
mugdhodzs38 <mugdhodzs38@gmail.com>
2024-10-02 08:34:41 +0000

    page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,14 @@
+import Cover from "@/components/Cover";
+import ProductCarousel from "@/components/ProductCarousel";
 
 
 export default function Home() {
-  return (<h1>Hello</h1>);
+  return (
+    <section className="px-4 md:px-8 py-4">
+      <Cover />
+      <div className="my-4">
+        <ProductCarousel />
+      </div>
+    </section>
+  );
 }
