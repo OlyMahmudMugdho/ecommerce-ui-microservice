35437f3d5211fdca439df335404d5356968016a7
M. Oly Mahmud <mugdhodzs38@gmail.com>
2024-10-04 16:52:46 +0600

    home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,18 @@
 import Cover from "@/components/Cover";
-import ProductCarousel from "@/components/ProductCarousel";
+import CategoriesSection from "@/components/homepage/CategoriesSection";
+import HeroSection from "@/components/homepage/HeroSection";
 
 
 export default function Home() {
   return (
     <section className="px-4 md:px-8 py-4">
       <Cover />
+      <HeroSection />
       <div className="my-4">
-        <ProductCarousel />
+        <h1 className="text-4xl text-center py-8 font-bold">
+          We sell all of these!
+        </h1>
+        <CategoriesSection />
       </div>
     </section>
   );
